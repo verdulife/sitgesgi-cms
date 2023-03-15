02a2c178a37e7d42ee05c17682f9d6825f82dfd5
verdulife <verdu@live.com>
2023-03-15 22:56:52 +0100

    startcss and postcss

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+import postcssPresetEnv from 'postcss-preset-env';
+import postcssEach from 'postcss-each';
+
+const config = {
+  plugins: [
+    postcssEach(),
+    postcssPresetEnv({
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
+        'custom-selectors': true
+      }
+    })
+  ]
+};
+
+export default config;
